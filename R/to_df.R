eb95aefd8701ba940411688d1fab5b9b4559129e
Cameron Harris <camharris22@gmail.com>
2021-03-10 17:21:42 -0700

    created initial version of to_df function

--- R/to_df.R
+++ R/to_df.R
@@ -1,16 +1,26 @@
 #' Change the formatting of data in column(s) of a dataframe to either human readable or numeric
 #'
-#' @param df dataframe, df to apply formatting
-#' @param col_names list or str, column names to change, default is all columns
-#' @param transform_type str, type of transformation
+#' @param df dataframe, dataframe to apply formatting
+#' @param col_names str or vector, column names to apply formatting (default is all columns)
+#' @param transform_type str, type of transformation (e.g. human, num)
 #' @param family str, family of suffix, numeric or filesize
 #'
 #' @return dataframe with formatting applied
 #' @export
 #'
-#' @examples to_df(df, col_names=['A', 'B', 'C'], transform_type='human')
+#' @examples to_df(df, col_names=c('A', 'B'), transform_type='human')
 #'
-#'
-to_df <- function(df, col_names, transform_type = 'human', family = "numeric") {
 
+to_df <- function(df, col_names = NULL, transform_type = 'human', family = "numeric") {
+  if (is.null(col_names)) {
+    col_names <-  colnames(df)
+  }
+
+  if (transform_type == 'human') {
+    df[] <- map_at(df, col_names, ~ to_human_fake(.))
+  } else if (transform_type == 'num') {
+    df[] <- map_at(df, col_names, ~ to_numeric(.))
+  }
+
+  return(df)
 }
